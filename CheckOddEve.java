ac5a1258d7b7f5839d57ca5e33f7f2c8fee78b59
Sajith-N <sajithreddy.n123@gmail.com>
2016-07-20 02:17:24 -0700

    Add files via upload

---
+++ CheckOddEve.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class CheckOddEve {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Scanner in=new Scanner(System.in);
+System.out.println("enter the number");
+int k=in.nextInt();
+if(k%2==0)
+{
+	System.out.println("even number");
+}
+else 
+{
+	System.out.println("odd number");
+}
+
+	}
+
+}
