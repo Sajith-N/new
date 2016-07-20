1caf4845771a78a44338f476a6b6a3cc6179584c
Sajith-N <sajithreddy.n123@gmail.com>
2016-07-20 02:09:39 -0700

    Create check.java

---
+++ check.java
@@ -0,0 +1,17 @@
+class Simple{
+public static void main(String[] args)
+{
+Scanner a=new Scanner(System.in);
+int no=a.nextInt();
+if(no==0)
+{
+System.out.println("zero");
+}
+else if(no>0)
+{
+System.out.println("positive");
+}
+else
+{
+System.out.println("negative");
+}
