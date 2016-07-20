47a90954aa0937d2ad2a3e774bf49f8f7c3f37b8
Sajith-N <sajithreddy.n123@gmail.com>
2016-07-20 03:30:12 -0700

    Add files via upload

---
+++ StringToInteger.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+public class StringToInteger {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner	in=new Scanner(System.in);
+String s=in.nextLine();
+int a=Integer.parseInt(s);
+System.out.println(a);
+	}
+
+}
