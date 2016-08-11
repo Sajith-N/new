56a90f03dcab8a4b3786e139f484ef2513bdd3b5
Sajith-N <sajithreddy.n123@gmail.com>
2016-08-11 14:05:52 +0530

    Create swaptwovalues.java

---
+++ swaptwovalues.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class test
+{
+public static void main(String[] args)
+{
+Scanner in=new Scanner(System.in);
+int a=in.nextInt();
+int b=in.nextInt();
+System.out.println("a="+a)'
+System.out.println("b="+b);
+System.out.println("after swapping");
+int temp;
+temp=a;
+a=b;
+b=temp;
+System.out.println("a="+a+"b="+b);
+}
+}
+
